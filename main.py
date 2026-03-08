19643c4fafb8ed67ef5f976353475221c8aab9e5
home <krishnan.ge@gmail.com>
2026-03-08 11:58:09 +0100

    Initial LBW app backend code

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def run_mobile_backend(video,corners):
     ret, frame_img = cap.read()
     cap.release()
     OFF_STUMP_X, LEG_STUMP_X, STUMP_HEIGHT, STUMP_BASE = stump_finder.find_stumps_overlay(frame_img, corners)
-    LEG_STUMP_X = 1114
+    pitch_width = corners[1]-corners[0]
+    LEG_STUMP_X = OFF_STUMP_X + 0.075*pitch_width
     print(str(OFF_STUMP_X),str(LEG_STUMP_X))
     # Stage 2: Pitch_finder uses the stumps to find the pitch of the ball
     BOUNCE_X, BOUNCE_Y, BOUNCE_FRAME = pitch_finder.find_pitch(video,corners, OFF_STUMP_X,LEG_STUMP_X,STUMP_HEIGHT,STUMP_BASE)
