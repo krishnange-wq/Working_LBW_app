97c356ee7b010f4c0835ae11dd3b5e80c37ff401
home <krishnan.ge@gmail.com>
2026-03-27 07:45:16 +0100

    Initial LBW app backend code

--- tracker.py
+++ tracker.py
@@ -160,10 +160,9 @@ def track_ball(video_path, OFF_STUMP_X,LEG_STUMP_X,STUMP_HEIGHT,STUMP_BASE,BOUNC
                         raw_k = (7 / 29) * STUMP_X_DIST
                         p_two = (10/29)*STUMP_X_DIST
                     else:
-                        raw_k = (5 / 29) * STUMP_X_DIST
+                        raw_k = (6.5 / 29) * STUMP_X_DIST -1
                         print("raw k"+str(raw_k))
-                        p_two  = (8.1/29)*STUMP_X_DIST
-                        p_two  = (9.1/29)*STUMP_X_DIST
+                        p_two  = (6.5/29)*STUMP_X_DIST +3
 
 
                     # Round it to the nearest ODD integer
